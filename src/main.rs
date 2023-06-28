88547b1b4d7a86a5d9b6c394bf4c1995ae70aa92
David Morrison <drmorr@evokewonder.com>
2023-06-28 11:29:07 -0700

    controller watches pods, reconcile loop called

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,48 @@
 use futures::{future, StreamExt};
 use k8s_openapi::api::core::v1::{Node, Pod};
 use kube::{
+    runtime::controller::{Action, Controller},
     runtime::{reflector, watcher, WatchStreamExt},
     Api, Client, ResourceExt,
 };
+use std::sync::Arc;
+use thiserror::Error;
+use tokio::time::Duration;
 use tracing::*;
-type Result<T, E = anyhow::Error> = std::result::Result<T, E>;
+type Result<T, E = MyError> = std::result::Result<T, E>;
+
+struct Context {}
+
+#[derive(Error, Debug)]
+pub enum MyError {
+    #[error("idkwtf")]
+    SomeDumbError,
+}
+
+async fn reconcile(pod: Arc<Pod>, ctx: Arc<Context>) -> Result<Action> {
+    info!("Got a pod: {}/{}", pod.name_any(), pod.namespace().unwrap());
+    return Ok(Action::await_change());
+}
+
+fn error_policy(pod: Arc<Pod>, error: &MyError, ctx: Arc<Context>) -> Action {
+    warn!("reconcile failed: {:?}", error);
+    Action::requeue(Duration::from_secs(5 * 60))
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(Level::DEBUG)
         .init();
-    let client = Client::try_default().await?;
+    let client = Client::try_default()
+        .await
+        .expect("failed to create kube client");
     let pods: Api<Pod> = Api::all(client.clone());
     let nodes: Api<Node> = Api::all(client.clone());
 
-    let (_, pod_writer) = reflector::store();
-    let (_, node_writer) = reflector::store();
-    let pod_watcher = reflector(pod_writer, watcher(pods, Default::default()))
-        .backoff(backoff::ExponentialBackoff::default())
-        .applied_objects()
-        .filter_map(|x| async move { Result::ok(x) })
-        .for_each(|o| {
-            debug!("Saw {} in {}", o.name_any(), o.namespace().unwrap());
-            future::ready(())
-        });
-
-    let node_watcher = reflector(node_writer, watcher(nodes, Default::default()))
-        .backoff(backoff::ExponentialBackoff::default())
-        .applied_objects()
-        .filter_map(|x| async move { Result::ok(x) })
-        .for_each(|o| {
-            debug!("Saw {}", o.name_any());
-            future::ready(())
-        });
-    tokio::select! {
-        _ = pod_watcher => warn!("watch exited"),
-        _ = node_watcher => warn!("watch exited"),
-    };
+    Controller::new(pods, watcher::Config::default())
+        .run(reconcile, error_policy, Arc::new(Context {}))
+        .for_each(|_| future::ready(()))
+        .await;
     Ok(())
 }
